054aeb6ce4f7d240ce1ec2af8c1d4fc67e57ad35
Balint Molnar <balintkilencvenegy@gmail.com>
2020-12-13 20:42:02 +0100

    Make amendments

--- src/components/observation/AddObservation.js
+++ src/components/observation/AddObservation.js
@@ -1,37 +1,74 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import qs from "qs";
 import styled from "styled-components";
 
-const AddObservation = () => {
-    const [userName, setUserName] = useState("");
-    const [description, setDescription] = useState("");
+const AddObservation = (props) => {
+  const [userName, setUserName] = useState("");
+  const [description, setDescription] = useState("");
 
-    useEffect(() => {
-        console.log(userName)
-    }, []);
+  useEffect(() => {
+    console.log(userName);
+  }, [userName]);
 
-    const submitHandler = () => {
-        setUserName(userName);
-        setDescription(description);
-    };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const url = `${process.env.REACT_APP_OBSERVATION_URL}/${props.city}`;
+    const bodyFormData = qs.stringify({
+      userName: userName,
+      description: description,
+    });
 
-    const nameInputChangeHandler = (event) => {
-        setUserName(event.target.value);
-    };
+    axios
+      .post(url, bodyFormData, {
+        "Content-Type": "application/x-www-form-urlencoded",
+      })
+      .then((res) => console.log(res));
+  };
 
-    const descriptionInputChangeHandler = (event) => {
-        setDescription(event.target.value);
-    };
+  const nameInputChangeHandler = (event) => {
+    setUserName(event.target.value);
+  };
 
-    return (
-    <React.Fragment>
-    <div>Name</div>
-      <input type="text" value={userName} onChange={nameInputChangeHandler}></input>
-    <div>Observation</div>
-      <input type="text" value={description} onChange={descriptionInputChangeHandler}></input>
-    <button onClick={submitHandler}>Submit</button>
-    </React.Fragment>
-    )
-}
+  const descriptionInputChangeHandler = (event) => {
+    setDescription(event.target.value);
+  };
+
+  return (
+    <ObservationForm>
+      <form method="post" onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="username">User name: </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={userName}
+            onChange={nameInputChangeHandler}
+          />
+        </div>
+        <div>
+          <label htmlFor="description">Description: </label>
+          <textarea
+            type="text"
+            rows="5"
+            cols="40"
+            id="description"
+            name="description"
+            value={description}
+            onChange={descriptionInputChangeHandler}
+          />
+        </div>
+        {/* <button onClick={submitHandler}>Submit</button> */}
+        <input type="submit" value="Send" />
+      </form>
+    </ObservationForm>
+  );
+};
+
+const ObservationForm = styled.div`
+  margin: 30px;
+  padding: 30px;
+`;
 
 export default AddObservation;
--- src/components/observation/Observations.js
+++ src/components/observation/Observations.js
@@ -4,46 +4,41 @@ import styled from "styled-components";
 import AddObservation from "./AddObservation";
 
 const Observations = (props) => {
-    const observationUrl = `${process.env.REACT_APP_OBSERVATION_URL}/${props.city}`;
-    const [state, setState] = useState({
-      observations: []
+  const observationUrl = `${process.env.REACT_APP_OBSERVATION_URL}/${props.city}`;
+  const [state, setState] = useState({
+    observations: [],
+  });
+
+  useEffect(() => {
+    axios.get(observationUrl).then((res) => {
+      setState({ observations: res.data });
     });
+  }, [observationUrl]);
 
-    useEffect(() => {
-      axios.get(observationUrl).then((res) => {
-        setState({ observations: res.data});
-      });
-    }, [observationUrl]);
-
-    return (
+  return (
     <React.Fragment>
+      <AddObservation city={props.city} />
       {console.log(state.observations)}
-      <h2 style={{ marginLeft: "60px", display: "flex" }}> User Observations</h2>
-      {state.observations.map(observation =>
-      <ObservationsContainer>
-        <ObservationItem>
-            {observation.userName}
-        </ObservationItem>
-        <ObservationItem>
-            {observation.timeStamp}
-        </ObservationItem>
-        <ObservationItem>
-            {observation.description}
-        </ObservationItem>
-      </ObservationsContainer>
-      )}
-      <AddObservation></AddObservation>
+      <h2 style={{ marginLeft: "60px", display: "flex" }}>
+        {" "}
+        User Observations
+      </h2>
+      {state.observations.map((observation) => (
+        <ObservationsContainer key={observation.id}>
+          <ObservationItem>{observation.userName}</ObservationItem>
+          <ObservationItem>{observation.timeStamp}</ObservationItem>
+          <ObservationItem>{observation.description}</ObservationItem>
+        </ObservationsContainer>
+      ))}
     </React.Fragment>
-    )
-}
+  );
+};
 
 // TODO: styling
 
 const ObservationsContainer = styled.div`
   margin-left: 60px;
 `;
-const ObservationItem = styled.div`
-  
-`;
+const ObservationItem = styled.div``;
 
 export default Observations;
